fd6c181b98dbb440a60e843aadce11f023ce23fd
chLi <565353780@qq.com>
2025-08-29 17:34:11 +0800

    add render triangle normal algo

--- mesh_sample/Demo/mesh_subdiver.py
+++ mesh_sample/Demo/mesh_subdiver.py
@@ -1,13 +1,13 @@
 import os
 import open3d as o3d
 
-from mesh_sample.Method.render import renderMeshEdges
+from mesh_sample.Method.render import renderMeshEdges, renderTriangleNormals
 from mesh_sample.Module.mesh_subdiver import MeshSubdiver
 
 
 def demo():
     mesh_file_path = "/Users/chli/chLi/Dataset/BitAZ/mesh/BitAZ.ply"
-    dist_max = 1.0 / 500
+    dist_max = 1.0 / 100
     # dist_max = float("inf")
     print_progress = True
     save_mesh_file_path = "/Users/chli/chLi/Dataset/BitAZ/subdiv_mesh/BitAZ.ply"
@@ -23,4 +23,5 @@ def demo():
     subdiv_mesh = mesh_subdiver.createSubdivMesh(save_mesh_file_path)
 
     renderMeshEdges(subdiv_mesh)
+    renderTriangleNormals(subdiv_mesh, 0.001)
     return True
--- mesh_sample/Method/render.py
+++ mesh_sample/Method/render.py
@@ -48,3 +48,41 @@ def renderMeshEdges(mesh: o3d.geometry.TriangleMesh) -> bool:
     o3d.visualization.draw_geometries([mesh, lineset])
 
     return True
+
+
+def renderTriangleNormals(
+    mesh: o3d.geometry.TriangleMesh,
+    normal_length: float = 0.01,
+) -> bool:
+    mesh.compute_triangle_normals()
+
+    # 获取三角形的中心点
+    triangles = np.asarray(mesh.triangles)
+    vertices = np.asarray(mesh.vertices)
+    triangle_centers = np.mean(vertices[triangles], axis=1)
+
+    # 获取法向量
+    triangle_normals = np.asarray(mesh.triangle_normals)
+
+    # 创建法向箭头（小线段）用于可视化
+    line_set = o3d.geometry.LineSet()
+
+    points = []
+    lines = []
+    colors = []
+
+    for i in range(len(triangle_centers)):
+        start = triangle_centers[i]
+        end = start + triangle_normals[i] * normal_length
+        points.append(start)
+        points.append(end)
+        lines.append([2 * i, 2 * i + 1])
+        colors.append([1, 0, 0])  # 红色箭头
+
+    line_set.points = o3d.utility.Vector3dVector(points)
+    line_set.lines = o3d.utility.Vector2iVector(lines)
+    line_set.colors = o3d.utility.Vector3dVector(colors)
+
+    # 可视化：网格 + 法向线段
+    o3d.visualization.draw_geometries([mesh, line_set])
+    return True
